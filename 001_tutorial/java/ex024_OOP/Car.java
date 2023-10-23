c9be0baa4a05f09d2d9708497464d2758529d882
Makuss88 <maciborek.grzegorz@gmail.com>
2023-10-23 22:00:50 +0200

    add ex24 about OPP, add file Garage and Car

---
+++ 001_tutorial/java/ex024_OOP/Car.java
@@ -0,0 +1,10 @@
+package ex024_OOP;
+
+public class Car {
+
+  String name;
+
+  Car(String name) {
+    this.name = name;
+  }
+}
---
+++ 001_tutorial/java/ex024_OOP/Garage.java
@@ -0,0 +1,8 @@
+package ex024_OOP;
+
+public class Garage {
+
+  void park(Car car) {
+    System.out.println("The " + car.name + " is parked");
+  }
+}
