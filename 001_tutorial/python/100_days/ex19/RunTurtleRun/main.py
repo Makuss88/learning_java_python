e04a54a5ed2036cc149c3d44c501d468b5eb33ba
Makuss88 <maciborek.grzegorz@gmail.com>
2024-01-02 12:28:23 +0100

    start project, now make a game

---
+++ 001_tutorial/python/100_days/ex19/RunTurtleRun/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import turtle as t
+
+tim = Turtle()
+screen = Screen()
+
+
+# def move_forwards():
+#     tim.forward(10)
+#
+#
+# def move_back():
+#     tim.back(10)
+#
+#
+# def move_left():
+#     new_heading = tim.heading() + 10
+#     tim.setheading(new_heading)
+#
+#
+# def move_right():
+#     new_heading = tim.heading() - 10
+#     tim.setheading(new_heading)
+#
+#
+# def clear():
+#     tim.home()
+#     tim.clear()
+#
+#
+# screen.listen()
+# screen.onkey(key='w', fun=move_forwards)
+# screen.onkey(key='s', fun=move_back)
+# screen.onkey(key='a', fun=move_left)
+# screen.onkey(key='d', fun=move_right)
+# screen.onkey(key='c', fun=clear)
+
+screen.exitonclick()
