4e492613174019b20bc89015fdddecdd5ac82fd9
Makuss88 <maciborek.grzegorz@gmail.com>
2024-01-05 23:12:29 +0100

    bad code..., and work...

--- 001_tutorial/python/100_days/ex22/pong/TODO.py
+++
@@ -1,8 +0,0 @@
-# TODO: add screen - okay
-# TODO: add class pong - okay
-# TODO: moving pong
-# TODO: add class ball
-# TODO: moving ball
-# TODO: add collision
-# TODO: collision out
-# TODO: score
--- 001_tutorial/python/100_days/ex22/pong/paddle.py
+++ 001_tutorial/python/100_days/ex22/pong/paddle.py
@@ -2,18 +2,19 @@ from turtle import Turtle
 
 
 class Paddle(Turtle):
-    def __init__(self, x_pos, y_pos):
+    
+    def __init__(self, position):
         super().__init__()
-        self.shape('square')
+        self.shape("square")
+        self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.color('white')
         self.penup()
-        self.goto(x=x_pos, y=y_pos)
+        self.goto(position)
 
-    def up(self):
-        y_cor = self.ycor()
-        self.setposition((self.xcor(), y_cor + 20))
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
-    def down(self):
-        y_cor = self.ycor()
-        self.setposition((self.xcor(), y_cor - 20))
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
